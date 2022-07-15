c516d1d5236d9c3dc9b36b208a35095d3ebe75ba
Daniil <62614735+DaniilTimonov@users.noreply.github.com>
2022-07-15 12:42:29 +0300

    Доработка кода - работа со стилями элементов

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <h1>Todo list application</h1>
+      <h1>Задачи для выполнения</h1>
+
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Создать задачу"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="add-button" onClick={addTodo}>
+          добавить
+        </button>
+      </div>
+
+      {todos?.length > 0 ? (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div className="todo">
+              <li key={index}> {todo} </li>
+
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteTodo(todo);
+                }}
+              >
+                Удалить
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>Задачи отсутствуют</p>
+        </div>
+      )}
     </div>
   );
 };
